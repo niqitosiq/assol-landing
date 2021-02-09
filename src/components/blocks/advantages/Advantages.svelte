fa35f141eb8ca112d6c34c7389ec9b7281422f6c
niqitosiq <shtanko@insigne.ru>
2021-02-09 20:05:42 +0300

    Летающее описание

--- src/components/blocks/advantages/Advantages.svelte
+++ src/components/blocks/advantages/Advantages.svelte
@@ -84,6 +84,42 @@
       <div class="cupboard-image">
         <Image src="/img/advantages/cupboard.png" />
       </div>
+
+      <div class="services">
+        <div class="service">
+          <div class="service-wrapper">
+            <div class="image">
+              <Image src="/g/img/service/2.jpg" />
+            </div>
+            <div class="description">
+              <h3>Упаковка</h3>
+              <p>Мы уже X-лет на рынке. Используя этот опыт</p>
+            </div>
+          </div>
+        </div>
+        <div class="service">
+          <div class="service-wrapper">
+            <div class="image">
+              <Image src="/g/img/service/2.jpg" />
+            </div>
+            <div class="description">
+              <h3>Упаковка</h3>
+              <p>Мы уже X-лет на рынке. Используя этот опыт</p>
+            </div>
+          </div>
+        </div>
+        <div class="service">
+          <div class="service-wrapper">
+            <div class="image">
+              <Image src="/g/img/service/2.jpg" />
+            </div>
+            <div class="description">
+              <h3>Упаковка</h3>
+              <p>Мы уже X-лет на рынке. Используя этот опыт</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </div>
@@ -123,5 +159,82 @@
     right: calc(100% - 20px);
     top: 144px;
     z-index: 8;
+  }
+
+  .services {
+    position: absolute;
+    left: calc(100% - 100px);
+    top: 104px;
+    z-index: 15;
+  }
+
+  .service {
+    margin-top: -5px;
+    width: 368px;
+    height: 159px;
+    padding: 5px;
+    position: relative;
+    &-wrapper {
+      height: 147px;
+      display: flex;
+      position: relative;
+      align-items: center;
+      z-index: 2;
+      transition: transform ease 0.3s;
+      &:after {
+        content: '';
+        box-sizing: border-box;
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        left: 0;
+        top: 0;
+        border: 1px solid #e3e3e3;
+        border-radius: 4px;
+        pointer-events: none;
+      }
+    }
+    &:after,
+    &:before {
+      content: '';
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      background-color: #fff;
+      border-radius: 5px;
+      border-radius: 5px;
+      z-index: 1;
+    }
+    &:after {
+      box-shadow: 0px 5.58354px 19.5424px rgba(0, 0, 0, 0.05);
+      left: 0;
+      top: 0;
+      transition: transform ease 0.3s;
+      transform: translateY(0px);
+    }
+    &:before {
+      box-shadow: 0px 3.08491px 79.705px rgba(0, 0, 0, 0.1);
+      top: 5px;
+      left: 5px;
+      width: calc(100% - 10px);
+      transition: transform ease 0.3s;
+      transform: translateY(0px);
+      transform-origin: center bottom;
+    }
+    &:hover {
+      &:after,
+      .service-wrapper {
+        transform: translateY(-5px);
+      }
+      &:before {
+        transform: translateY(-4px);
+      }
+    }
+    .image {
+      flex-shrink: 0;
+      width: 170px;
+      height: 100%;
+      margin-right: 26px;
+    }
   }
 </style>
