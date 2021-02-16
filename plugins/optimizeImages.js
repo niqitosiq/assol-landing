f5de7dfd045f5aef8964e449b7f3549be93feaf1
niqitosiq <shtanko@insigne.ru>
2021-02-16 13:51:56 +0300

    ImageMin config

--- plugins/optimizeImages.js
+++ plugins/optimizeImages.js
@@ -1,55 +1,70 @@
-import { buffer as imagemin } from 'imagemin';
+import imagemin from 'imagemin';
+import imageminWebp from 'imagemin-webp';
+import imageminJpegtran from 'imagemin-jpegtran';
+import imageminPngquant from 'imagemin-pngquant';
+import imageminZopfli from 'imagemin-zopfli';
+import imageminMozjpeg from 'imagemin-mozjpeg';
+
 import util from 'util';
 import fs from 'fs';
 import path from 'path';
-import klawSync from 'klaw-sync';
-import mkpath from 'mkpath';
+import makeDir from 'make-dir';
 
-const extensionsRe = /\.(svg|png|jpg|gif|webp)$/;
-
-const mkpathAsync = util.promisify(mkpath);
-const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 
 export default function optimizeImages({
-  plugins = [],
-  images = './src/images/',
+  images = './src/images',
   staticFolder = './static/img/',
 } = {}) {
-  const absolutePath = path.resolve(images);
   return {
     name: 'imagemin-plugin',
-    async generateBundle(_, bundle) {
-      const imagePaths = klawSync(absolutePath, {
-        filter: file => extensionsRe.test(file.path),
-        nodir: true,
-        traverseAll: true,
-      });
+    async generateBundle() {
+      const imageMins = [];
 
-      await Promise.all(
-        imagePaths.map(async item => {
-          const file = fs.readFileSync(item.path);
-          item.source = await imagemin(file, { plugins });
-        }),
+      imageMins.push(
+        imagemin([`${images}/**/*.png`], {
+          plugins: [imageminWebp({ quality: 70 })],
+        }).then(files =>
+          files.forEach(async v => {
+            let source = path.parse(v.sourcePath);
+            v.destinationPath = `${source.dir.replace(images, staticFolder)}/${
+              source.name
+            }.webp`;
+            await makeDir(path.dirname(v.destinationPath));
+            await writeFile(v.destinationPath, v.data);
+          }),
+        ),
+      );
+      imageMins.push(
+        imagemin([`${images}/**/*.{jpg,png}`], {
+          plugins: [
+            imageminZopfli({
+              more: true,
+            }),
+            imageminPngquant({
+              speed: 1,
+              quality: [0.95, 1],
+            }),
+            imageminJpegtran({
+              progressive: true,
+            }),
+            imageminMozjpeg({
+              quality: 90,
+            }),
+          ],
+        }).then(files =>
+          files.forEach(async v => {
+            let source = path.parse(v.sourcePath);
+            v.destinationPath = `${source.dir.replace(images, staticFolder)}/${
+              source.name
+            }${source.ext}`;
+            await makeDir(path.dirname(v.destinationPath));
+            await writeFile(v.destinationPath, v.data);
+          }),
+        ),
       );
 
-      const writings = [];
-      imagePaths.forEach(image => {
-        const imagePath = path.relative(absolutePath, image.path);
-        const fullPath = path.resolve(staticFolder, imagePath);
-        const staticImageDir = path.dirname(fullPath);
-
-        writings.push(
-          mkpathAsync(staticImageDir).then(() => {
-            return writeFile(fullPath, image.source).catch(error => {
-              this.error(
-                `Couldn't write optimized input buffer for ${image.path}: ${error}`,
-              );
-            });
-          }),
-        );
-      });
-      await Promise.all(writings);
+      await Promise.all(imageMins);
     },
   };
 }
