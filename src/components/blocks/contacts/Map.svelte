363f4cef2663767b646e78396fb46200bcc9887b
niqitosiq <shtanko@insigne.ru>
2021-02-21 12:25:35 +0300

    Лейзи лоадинг для карты

--- src/components/blocks/contacts/Map.svelte
+++ src/components/blocks/contacts/Map.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { onMount } from 'svelte';
+  import viewport from '../../utils/viewport';
 
   export let locations = [
     {
@@ -11,6 +11,28 @@
 
   export let center = [55.75361503443606, 37.620883000000006];
   export let zoom = 17;
+
+  let loaded = false;
+
+  function loadScript(url, callback) {
+    var script = document.createElement('script');
+
+    if (script.readyState) {
+      script.onreadystatechange = function () {
+        if (script.readyState == 'loaded' || script.readyState == 'complete') {
+          script.onreadystatechange = null;
+          callback();
+        }
+      };
+    } else {
+      script.onload = function () {
+        callback();
+      };
+    }
+
+    script.src = url;
+    document.getElementsByTagName('head')[0].appendChild(script);
+  }
 
   function loadMap() {
     var myMap = new ymaps.Map('map', {
@@ -37,34 +59,33 @@
   }
 
   const initMap = () => {
-    if (!window.ymaps) {
-      setTimeout(initMap, 250);
-      return;
-    }
-
-    ymaps.ready(() => {
-      loadMap();
-    });
+    loadScript(
+      'https://api-maps.yandex.ru/2.1/?lang=ru_RU&amp;apikey=cf1b8beb-bb0c-4563-9d28-c603002dd2ad&load=Map,Placemark,geoObject.addon.balloon',
+      () => {
+        ymaps.ready(() => {
+          loadMap();
+          loaded = true;
+        });
+      },
+    );
   };
-
-  onMount(() => {
-    initMap();
-  });
 </script>
 
-<svelte:head>
-  <script
-    src="https://api-maps.yandex.ru/2.1/?lang=ru_RU&amp;apikey=cf1b8beb-bb0c-4563-9d28-c603002dd2ad&load=Map,Placemark,geoObject.addon.balloon"
-    type="text/javascript"
-    defer>
-  </script>
-</svelte:head>
+<div class="map-wrapper" class:loaded>
+  <div id="map" use:viewport on:enterViewport={initMap} />
+</div>
 
-<div id="map" />
-
-<style>
+<style lang="scss">
+  .map-wrapper,
   #map {
     width: 100%;
     height: 100%;
+  }
+  .map-wrapper {
+    background-image: url('/img/map-big.jpg');
+    background-size: cover;
+    &.loaded {
+      background-image: none;
+    }
   }
 </style>
